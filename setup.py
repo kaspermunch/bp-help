ba1025ce876799f0ef53ce9c868e9e5141b15fc7
Kasper Munch <kaspermunch@birc.au.dk>
2024-08-29 20:12:38 +0200

    update

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from setuptools import setup, find_packages
 
 setup(name='bp_help',
       test_suite='tests',
-      version='1.0.17',
+      version='1.0.18',
       description='',
       long_description='',
       author='Kasper Munch',
